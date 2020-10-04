8ed410095cf555aa21b0356e9be5c2a7ac09594b
GamerNoTitle <bili33@87ouo.top>
2020-10-04 14:25:52 +0800

    添加钉钉表情包

---
+++ dingtalk/BQB-Link-List-Generate.py
@@ -0,0 +1,25 @@
+import os
+remotepath = 'https://valinecdn.bili33.top/'
+prefix= input("请输入前缀：")
+def findAllFile(base):
+    for root, ds, fs in os.walk(base):
+        for f in fs:
+            yield f
+
+def main():
+    base = './'
+    linklist=[]
+    num=1
+    print('```json')
+    print('{')
+    for i in findAllFile(base):
+        print('\"{}{}\": \"{}/{}\",'.format(prefix,num,prefix,i))
+        num=num+1
+    print('}')
+    print('```')
+    num=1
+    for i in findAllFile(base):
+        print('![{}{}]({}{}/{})'.format(prefix,num,remotepath,prefix,i))
+        num=num+1
+if __name__ == '__main__':
+    main()
