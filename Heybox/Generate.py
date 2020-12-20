93405a16a13571fa6f24b1037413f69971496d0d
GamerNoTitle <bili33@87ouo.top>
2020-12-20 12:18:39 +0800

    加入小黑盒表情包并重写脚本

---
+++ Heybox/Generate.py
@@ -0,0 +1,53 @@
+import os
+remotepath = 'https://valinecdn.bili33.top/'
+prefix= input("请输入前缀：")
+FilePath="."
+def findAllFile(base):
+    for root, ds, fs in os.walk(base):
+        for f in fs:
+            if f!='Generate.py':
+                yield f
+
+def walkFile(FilePath):
+    print("正在生成MiniValine所需的index.json文件")
+    S='''{"0":['''
+    for root, dirs, files in os.walk(FilePath):
+        for f in files:
+            if f!='Generate.py':
+                Path=os.path.join(root, f)
+                S+='"'+f+'",'
+    S+="]}"
+    S=S.replace(",]}", "]}")
+    print("正在写入文件，这通常不会太久...")
+    with open("./index.json","wb") as ff:
+        ff.write(S.encode("utf-8"))
+    print("恭喜，已成功完成")
+
+def main():
+    base = './'
+    linklist=[]
+    num=1
+    print('# {}'.format(prefix))
+    print('MiniValine:')
+    print('> https://valinecdn.bili33.top/{}'.format(prefix))
+    print('```json')
+    print('{')
+    for i in findAllFile(base):
+        if i!='Generate.py':
+            print('\"{}{}\": \"{}/{}\",'.format(prefix,num,prefix,i))
+            num=num+1
+    print('}')
+    print('```')
+    num=1
+    for i in findAllFile(base):
+        if i!='Generate.py':
+            print('![{}{}]({}{}/{})'.format(prefix,num,remotepath,prefix,i))
+            num=num+1
+if __name__ == '__main__':
+    main()
+    print("请坐和放宽，我们正帮你搞定一切......")
+    try:
+        walkFile(FilePath)
+    except Exception as e:
+        print("生成失败！我们都有不顺利的时候.")
+        print(e)
